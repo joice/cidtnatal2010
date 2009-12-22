e405ba57812b5606a7e99a8a8b94a0938faef978
Joice Taciana <joicetaciana@gmail.com>
2009-12-22 13:05:34 -0300

    changing cidt countdown to match event's date

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
   $("a[href^='http:']").not("[href*='localhost']").attr('target','_blank');
-  var difference = (new Date("26/05/2010") - new Date()) / 1000;
+  var difference = (new Date("05/26/2010") - new Date()) / 1000;
   $("#countdown").html(Math.floor(( difference < 0 ? 0 : difference ) / (60 * 60 * 24)) + ' Dias');
   $("#scroller").scrollable().circular().mousewheel().autoscroll({ 
     autoplay: true
