f32c8f222bd5f4d5c169317199fd3af7e5b9e696
Joice Taciana <joicetaciana@gmail.com>
2010-03-09 13:52:36 -0300

    rename the javascript, it no longer needs to be included in the juicer

--- js/_theme.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($){
-  $("a[href^='http:']").not("[href*='localhost']").attr('target','_blank');
-  var difference = (new Date("05/26/2010") - new Date()) / 1000;
-  $("#countdown").html(Math.floor(( difference < 0 ? 0 : difference ) / (60 * 60 * 24)) + ' Dias');
-  $("#scroller").scrollable().circular().mousewheel().autoscroll({ 
-    autoplay: true
-  });
-});
